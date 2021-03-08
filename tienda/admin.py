d7e11dd4b13f6956c577b0cc4599671fd73161e6
josesaucedo502-alt <79068003+josesaucedo502-alt@users.noreply.github.com>
2021-03-08 18:32:35 -0300

    Add files via upload

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from tienda.models import Ciudades, AtractivosTuristicos
 # Register your models here.
+
+class CiudadesAdmin(admin.ModelAdmin):
+	readonly_fields=('creacion', 'actualizacion')
+
+class TuristicoAdmin(admin.ModelAdmin):
+	readonly_fields=('creacion', 'actualizacion')
+
+
+admin.site.register(Ciudades, CiudadesAdmin)
+admin.site.register(AtractivosTuristicos, TuristicoAdmin)
