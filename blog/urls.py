469e904fef42f5f150d2819a329493b61d40c180
[fabi] <[nestorfabi678@gmail.com]>
2021-03-08 16:28:55 -0300

    creación de blog

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from blog.views import blog, categoria
+
+
+urlpatterns = [
+    path('', blog, name="Blog"),
+    path('categoria/<int:categoria_id>/', categoria, name="categoria"),
+]
+from django.urls import path
+from blog.views import blog, categoria
+
+
+urlpatterns = [
+    path('', blog, name="Blog"),
+    path('categoria/<int:categoria_id>/', categoria, name="categoria"),
+]
