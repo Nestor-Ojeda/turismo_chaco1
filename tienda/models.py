26f9994c2943fbf5d31ee0198e37a9902362c46b
josesaucedo502-alt <79068003+josesaucedo502-alt@users.noreply.github.com>
2021-03-08 18:41:38 -0300

    Add files via upload

--- tienda/models.py
+++ tienda/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Ciudades(models.Model):
+    codigo_postal=models.CharField(max_length=50)
+    ciudad=models.CharField(max_length=50)
+    zona_turistica=models.BooleanField(primary_key=False)
+    codigo_postal=models.IntegerField()
+    creacion=models.DateTimeField(auto_now_add=True)
+    actualizacion=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name="ciudad"
+        verbose_name_plural="ciudades"
+
+    def __str__(self):
+        return self.ciudad
+
+
+class AtractivosTuristicos(models.Model):
+    nombre=models.CharField(max_length=50)
+    ciudad_origen=models.ForeignKey(Ciudades, on_delete=models.CASCADE)
+    imagen=models.ImageField(upload_to="tienda", null=True, blank=True)
+    horarios=models.CharField(max_length=50)
+    precio_entrada=models.FloatField()
+    creacion=models.DateTimeField(auto_now_add=True)
+    actualizacion=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name="AtractivoTuristico"
+        verbose_name_plural="AtractivosTuristicos"
+
+    def __str__(self):
+        return self.nombre
--- tienda/views.py
+++ tienda/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from tienda.models import Ciudades, AtractivosTuristicos
 
 # Create your views here.
+def tienda(request):
+	ciudades=Ciudades.objects.all()
+	turistico=AtractivosTuristicos.objects.all()
+	return render(request, "tienda/tienda.html", {"ciudades": ciudades, "turistico": turistico})
